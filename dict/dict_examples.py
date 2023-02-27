b791a5c88ce40c6be9a7c33c8ec7496db32cc583
camila-marquess <camilamarquesoliveira@outlook.com>
2023-02-27 11:20:39 -0300

    feat: applying function to dicts

--- dict/dict_examples.py
+++ dict/dict_examples.py
@@ -59,3 +59,64 @@ CAT_DICT.update({"color":"gray"})
 CAT_DICT.items()
 
 # %%
+
+'''
+
+Updating a dict value
+
+'''
+  
+for key, value in CAT_DICT.items():
+  if key == 'color':
+    CAT_DICT[key] = 'gray' 
+  
+CAT_DICT.items()
+# %%
+
+'''
+
+List comprehension in dicts
+
+'''
+
+new_dict = {k: v for k, v in CAT_DICT.items() if k == 'age'}
+
+print(new_dict)
+
+# %%
+
+'''
+
+Using functions in dicts
+
+'''
+
+prices = {'apple': 0.40, 'orange': 0.35, 'banana': 0.25}
+
+def total_value(current_price):
+  return (current_price[0], current_price[1] * 10)
+
+final_value = dict(map(total_value, prices.items()))
+
+print(final_value)
+
+# %%
+
+'''
+
+Using toolz library
+
+'''
+
+prices = {'apple': 0.40, 'orange': 0.35, 'banana': 0.25}
+
+import toolz 
+
+def total_value(current_price):
+  return (current_price * 10)
+
+final_value = toolz.valmap(total_value, prices)
+
+print(final_value)
+
+# %%
